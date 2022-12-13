762fc284afdf360ae05e41c703c0b4f88b2a154c
Felipe099 <lipepagodart99@gmail.com>
2022-12-12 22:06:07 -0300

    :sparkles: add version mobile

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
 function enviar() {
     let valorValue = document.querySelector('.js-input-valor').value;
     let vezesValue = document.querySelector('.js-input-vezes').value;
-    
 
     if (vezesValue == 1) {
-      jurosValue = 0.7;
+        jurosValue = 1.07;
     } else if (vezesValue >= 2 && vezesValue <= 6) {
-      jurosValue = 1.1;
+        jurosValue = 1.11;
     } else if (vezesValue >= 7 && vezesValue <= 10) {
-      jurosValue = 1.4;
+        jurosValue = 1.14;
     } else if (vezesValue >= 11 && vezesValue <= 12) {
-      jurosValue = 1.6;
+        jurosValue = 1.16;
     } else {
-      jurosValue =  2.5;
+        jurosValue = 1.25;
     }
 
     let mult = valorValue * jurosValue;
     let total = mult / vezesValue;
-    let texto = total;
 
     if (valorValue == 0 || valorValue == '') {
         document.getElementById(
-            'paragrafo'
+            'valueParcela'
         ).innerHTML = `Digite o valor para fazer a simulção`;
-    } else if (vezesValue == 0|| valorValue == '') {
+    } else if (vezesValue == 0 || valorValue == '') {
         document.getElementById(
-            'paragrafo'
+            'valueParcela'
         ).innerHTML = `Digite a quantidade de vezes`;
     } else {
+      document.getElementById(
+          'valueTotal'
+      ).innerHTML = `Valor total: R$ ${mult.toFixed(2)} reais`;
         document.getElementById(
-            'paragrafo'
-        ).innerHTML = `Valor das parcelas: R$ ${texto.toFixed(2)} reais`;
+            'valueParcela'
+        ).innerHTML = `Valor das parcelas: R$ ${total.toFixed(2)} reais`;
     }
 }
